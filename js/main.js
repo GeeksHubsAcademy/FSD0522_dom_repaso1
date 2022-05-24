77f5eb862d8ad2ce48d7186e0620c9a9ab8231eb
Dave86dev <davidochando1986@gmail.com>
2022-05-24 10:30:10 +0200

    Uso de DOM kick off

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// let miDivFavorito = document.getElementById("nieto");
+
+// let pais = prompt("Dime el nombre del pais");
+
+// miDivFavorito.innerHTML = pais;
+
+let objetivo = prompt("Dime que Div quieres quitar");
+
+document.getElementById(objetivo).style.display = "none";
